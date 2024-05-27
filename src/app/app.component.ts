84e4e23e78b69735514ad70f1bf7954a5df484d7
Reshander <you@example.comreshander230502@gmail.com>
2024-05-27 17:21:31 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TodoComponent } from './component/todo/todo.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,TodoComponent,FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'todo-list';
+
 }
---
+++ src/app/component/todo/todo.component.ts
@@ -0,0 +1,32 @@
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-todo',
+  standalone: true,
+  imports: [FormsModule, CommonModule],
+  templateUrl: './todo.component.html',
+  styleUrl: './todo.component.css',
+})
+export class TodoComponent implements OnInit {
+  taskArray = [{ taskName: 'Bathing', isCompleted: false }];
+taskForm: any;
+  constructor() {}
+  ngOnInit(): void {}
+
+  onSubmit(form: NgForm) {
+    console.log(form);
+
+    this.taskArray.push({
+      taskName: form.controls['task'].value,
+      isCompleted: false,
+    });
+    form.reset()
+  }
+
+  onDelete(index: number) {
+    console.log(index);
+    this.taskArray.splice(index, 1);
+  }
+}
